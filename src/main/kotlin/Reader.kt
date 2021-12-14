873d3f8d50d7877c5bf5d77b3d5531c903085596
Calvin Li <calvinliqh@gmail.com>
2021-12-13 18:14:32 -0800

    day 3 part 2

---
+++ src/main/kotlin/Reader.kt
@@ -0,0 +1,12 @@
+import java.io.File
+
+@Suppress("unused")
+class Reader(filename: String) {
+    private val bufferedReader = File(filename).inputStream().bufferedReader()
+
+    fun readLine(): String = bufferedReader.readLine()
+    fun readLines(): List<String> = bufferedReader.readLines()
+
+    fun forEachLine(foo: (input: String) -> Unit) =
+        bufferedReader.forEachLine {foo(it)}
+}
