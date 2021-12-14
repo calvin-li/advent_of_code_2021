10bce98c5120ad3883d6259fb16859bdf4b1430a
Calvin Li <calvinliqh@gmail.com>
2021-12-14 01:42:29 -0800

    day 6 part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,53 +5,26 @@ val input = Reader(if (System.getenv("TEST") == "True") testFile else filename)
 
 @Suppress("UNUSED_PARAMETER")
 fun main(args: Array<String>) {
-    val lines = input.readLines()
-    val vents = mutableListOf<IntArray>()
-    lines.forEach {line ->
-        vents.add(line.replace(" -> ", ",").split(',').map { it.toInt() }.toIntArray())
-    }
+    val SPAWN = 7
+    val DELAY = 2
+    val DAYS = 80
 
-    val xMax = 1 + vents.maxOf { it.filterIndexed { index, _ -> index % 2 == 0 }.maxOf{i->i} }
-    val yMax = 1 + vents.maxOf { it.filterIndexed { index, _ -> index % 2 == 1 }.maxOf{i->i} }
+    val fish = Array(DAYS){ IntArray(SPAWN+DELAY) }
+    fish[0][0] = 2
+    for(i in 1 until fish[0].count()){ fish[0][i] = 1}
 
-    val smoke = Array<IntArray>(yMax){ IntArray(xMax) }
-
-    vents.forEach { vent ->
-        val x1 = vent[0]
-        val y1 = vent[1]
-        val x2 = vent[2]
-        val y2 = vent[3]
-
-        getRange(x1, y1, x2, y2).forEach{
-            smoke[it.second][it.first]++
+    for(i in 1 until fish.count()){
+        val prev = fish[i - 1]
+        fish[i][0] = prev[6] + prev[8]
+        for(j in 1 until prev.count()){
+            fish[i][j] = prev[j-1]
         }
     }
-    println(smoke.sumOf { it.count { j -> j>1 }})
-}
 
-fun getRange(x1: Int, y1: Int, x2: Int, y2: Int): List<Pair<Int, Int>> {
-    if (x1 == x2){
-        val r = if(y1 <= y2){(y1..y2)}else{(y1 downTo y2)}
-        return r.map { Pair(x1, it) }
+    var total = 0
+    input.readLine().split(',').forEach {
+        total += fish.last()[it.toInt()]
     }
-    else if (y1 == y2){
-        val r = if(x1 <= x2){(x1..x2)}else{(x1 downTo x2)}
-        return r.map { Pair(it, y1) }
-    }
-    else{
-        var start = Pair(x1, y1)
-        var end = Pair(x2, y2)
-        if(x1 > x2){
-            val temp = start
-            start = end
-            end = temp
-        }
-        return (start.first..end.first).mapIndexed { index, x ->
-            if(start.second < end.second){
-                Pair(x, start.second + index)
-            }else{
-                Pair(x, start.second - index)
-            }
-        }
-    }
+
+    println(total)
 }
